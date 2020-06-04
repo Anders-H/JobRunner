4ff478707f789be8dd464789492b637015cd3677
Anders Hesselbom <anders.hesselbom@bonliva.se>
2020-06-04 19:15:20 +0200

    In-process async structure in progress.

--- JobRunner/ObjectModel/InProcess/InProcessJob.cs
+++ JobRunner/ObjectModel/InProcess/InProcessJob.cs
@@ -1,12 +1,14 @@
-﻿using JobRunner.Services;
+﻿using System;
+using JobRunner.Services;
 
 namespace JobRunner.ObjectModel.InProcess
 {
     public abstract class InProcessJob
     {
-        public abstract bool HasExited { get; }
-        public abstract int ExitCode { get; }
+        public bool HasExited { get; protected set; }
+        public int ExitCode { get; protected set; }
         public abstract void Begin(ArgumentList args);
         public abstract void Kill();
+        protected IAsyncResult AsyncResult { get; set; }
     }
 }
--- JobRunner/ObjectModel/InProcess/Jobs/DownloadStringJob.cs
+++ JobRunner/ObjectModel/InProcess/Jobs/DownloadStringJob.cs
@@ -1,20 +1,55 @@
-﻿using JobRunner.Services;
+﻿using System;
+using System.IO;
+using System.Threading;
+using JobRunner.Services;
 
 namespace JobRunner.ObjectModel.InProcess.Jobs
 {
     public class DownloadStringJob : InProcessJob
     {
-        public override bool HasExited { get; }
-        public override int ExitCode { get; }
-
+        private string SourceUrl { get; set; }
+        private string TargetFile { get; set; }
+        private string ExistsBehavour { get; set; }
+        private Action<string, string> Action { get; set; }
+        
         public override void Begin(ArgumentList args)
         {
-            throw new System.NotImplementedException();
+            SourceUrl = args.GetAfter("-source");
+            TargetFile = args.GetAfter("-target");
+            ExistsBehavour = args.GetAfter("-existsbehaviour");
+
+            switch (ExistsBehavour.ToLower())
+            {
+                case "skip":
+                    if (File.Exists(TargetFile))
+                    {
+                        HasExited = true;
+                        ExitCode = 0;
+                        return;
+                    }
+                    break;
+                case "fail":
+                    if (File.Exists(TargetFile))
+                    {
+                        HasExited = true;
+                        ExitCode = 1;
+                        throw new SystemException("Target file exists.");
+                    } 
+                    break;
+            }
+            HasExited = false;
+            Action = new Action<string, string>(DownloadString);
+            AsyncResult = Action.BeginInvoke(SourceUrl, TargetFile, null, null);
+        }
+
+        private void DownloadString(string source, string target)
+        {
+            Thread.Sleep(5000);
         }
         
         public override void Kill()
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
