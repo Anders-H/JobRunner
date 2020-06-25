565595bad231897605795c6ca8635d52e210711e
Anders-H <anders@winsoft.se>
2020-06-25 17:51:16 +0200

    Placeholder for delete class.

---
+++ JobRunner/ObjectModel/InProcess/Jobs/DeleteFileJob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JobRunner.Services;
+
+namespace JobRunner.ObjectModel.InProcess.Jobs
+{
+    public class DeleteFileJob : InProcessJob
+    {
+        private string FileToDelete { get; set; }
+        private string FileNotFoundBehavour { get; set; }
+
+        public override void Begin(ArgumentList args)
+        {
+
+        }
+
+
+
+
+
+
+
+        protected override void CompletedCallback(IAsyncResult result)
+        {
+            HasExited = true;
+            ExitCode = Exception == null
+                ? 0
+                : -1;
+        }
+    }
+}
